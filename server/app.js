af280bad0275566f51b5c7a0fbc839c8f2347c0b
Tomas Smolko <tomas.smolko@student.tuke.sk>
2025-10-06 23:51:48 +0200

    feat: Simple data streaming setup

--- server/app.js
+++ server/app.js
@@ -3,22 +3,62 @@ const fetchRepoData = require('./utils/fetchRepoData.js');
 const parseCSV = require('./utils/parseCSV.js');
 
 const app = express();
-
 const port = process.env.PORT || 8080;
 
-app.use(express.json());
+app.use((req, res, next) => {
+    res.set({
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    next();
+});
 
-app.get('/', async (req, res) => {
+let initData;
 
+async function initializeData() {
     try {
         const scraperDataObject = await fetchRepoData();
-        const parsedScraperData = await parseCSV(scraperDataObject.content);
-        console.log(parsedScraperData);
+        initData = await parseCSV(scraperDataObject.content);
     } catch (e) {
-        res.status(500).send("Error getting csv data: " + e);
+        console.error("Failed to initialize show data: " + e);
     }
+}
+
+function streamData(res) {
+    setInterval(() => {
+        res.write("data: test\n\n");
+    }, 1000);
+}
+
+app.get('/dajto', (req, res) => {
+
+    if (!initData) {
+        res.status(503).send("Data not initialized on server");
+        return;
+    }
+    streamData(res);
 })
 
-app.listen(port, () => {
+app.get('/prima-sk', (req, res) => {
+
+    if (!initData) {
+        res.status(503).send("Data not initialized on server");
+        return;
+    }
+    streamData(res);
+})
+
+app.get('/markiza-krimi', (req, res) => {
+
+    if (!initData()) {
+        res.status(503).send("Data not initialized on server");
+        return;
+    }
+    streamData(res);
+})
+
+app.listen(port, async () => {
+    await initializeData();
     console.log(`\nServer is running on http://localhost:${port}`);
 });
